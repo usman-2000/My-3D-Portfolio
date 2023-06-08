809fdced5c251a7411445c49dee2771b27cc295c
usmanrahim <usmanrahim2000@gmail.com>
2023-06-08 13:27:33 +0500

    3D modeling

--- 3D portfolio Code/src/components/canvas/Computers.jsx
+++ 3D portfolio Code/src/components/canvas/Computers.jsx
@@ -10,21 +10,38 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        intensity={1}
+        penumbra={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -4.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
 
-const ComputerCanvas = () => {
+const ComputersCanvas = () => {
   return (
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [30, 5, 7], fov=25}}
-      gl={{preserveDrawingBuffer:true}}
+      camera={{ position: [30, 5, 7], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={CanvasLoader}>
-        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2} />
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
         <Computers />
       </Suspense>
       <Preload all />
@@ -32,4 +49,4 @@ const ComputerCanvas = () => {
   );
 };
 
-export default Computers;
+export default ComputersCanvas;
