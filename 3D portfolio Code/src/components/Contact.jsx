9ad720ee009318eea248cded796ed8760b8e1dbb
usmanrahim <usmanrahim2000@gmail.com>
2023-06-10 00:30:20 +0500

    New sections added

--- 3D portfolio Code/src/components/Contact.jsx
+++ 3D portfolio Code/src/components/Contact.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const fromRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const handleChange = () => {};
+  const handleSubmit = () => {};
+  return (
+    <div className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      ></motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
