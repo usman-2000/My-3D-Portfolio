cf3b8c3cdc22e498456f27bcc1644d08e7cfe12a
usmanrahim <usmanrahim2000@gmail.com>
2023-06-10 19:13:47 +0500

    New commit

--- 3D portfolio Code/src/components/Contact.jsx
+++ 3D portfolio Code/src/components/Contact.jsx
@@ -8,7 +8,7 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  const fromRef = useRef();
+  const formRef = useRef();
   const [form, setForm] = useState({
     name: "",
     email: "",
@@ -23,7 +23,52 @@ const Contact = () => {
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-      ></motion.div>
+      >
+        <p className={`${styles.sectionSubText}`}>Get in touch</p>
+        <h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
+        <form
+          action=""
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">
+              Your Email Address
+            </span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+        </form>
+      </motion.div>
     </div>
   );
 };
