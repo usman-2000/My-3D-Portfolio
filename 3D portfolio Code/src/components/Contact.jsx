0d64845dbe501b7c618b58ddb03bc31d037c518c
usmanrahim <usmanrahim2000@gmail.com>
2023-06-12 00:38:17 +0500

    ss

--- 3D portfolio Code/src/components/Contact.jsx
+++ 3D portfolio Code/src/components/Contact.jsx
@@ -44,30 +44,42 @@ const Contact = () => {
             />
           </label>
           <label htmlFor="" className="flex flex-col">
-            <span className="text-white font-medium mb-4">
-              Your Email Address
-            </span>
+            <span className="text-white font-medium mb-4">Your Email</span>
             <input
-              type="text"
-              name="name"
-              value={form.name}
+              type="email"
+              name="email"
+              value={form.email}
               onChange={handleChange}
-              placeholder="What's your name?"
+              placeholder="What's your email?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
           <label htmlFor="" className="flex flex-col">
-            <span className="text-white font-medium mb-4">Message</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
               onChange={handleChange}
-              placeholder="What's your name?"
+              placeholder="What do you want to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+          >
+            {loading ? "Sending..." : "Submit"}
+          </button>
         </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   );
